aa8c05b4c8b3a76de16f52ad1bc133452cedb79f
dbm0204 <dbm0204@gmail.com>
2017-07-11 14:35:39 +0530

    Initial Commit

---
+++ AndroidStudioProjects/BarCodeReader/app/src/main/java/com/example/dbm0204/barcodereader/AddProduct.java
@@ -0,0 +1,8 @@
+package com.example.dbm0204.barcodereader;
+
+/**
+ * Created by dbm0204 on 1/15/17.
+ */
+
+public class AddProduct {
+}
---
+++ AndroidStudioProjects/BarcodeReader/app/src/main/java/com/example/toyo/barcodereader/ProductDatabase.java
@@ -0,0 +1,8 @@
+package com.example.toyo.barcodereader;
+
+/**
+ * Created by dbm0204 on 1/15/17.
+ */
+
+public class ProductDatabase {
+}
